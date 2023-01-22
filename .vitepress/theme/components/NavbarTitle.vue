c68430af29dd6ae66a635ac9d8b921725a8882e3
johnsoncodehk <johnsoncodehk@gmail.com>
2023-01-22 21:50:55 +0800

    feat: add logo

--- .vitepress/theme/components/NavbarTitle.vue
+++ .vitepress/theme/components/NavbarTitle.vue
@@ -1,10 +1,19 @@
 <template>
     <a class="VPNavBarTitle" href="/">
         <slot name="navbar-title">
-            <!-- <svg class="logo" viewBox="0 0 128 128" width="24" height="24">
-                <path fill="#42b883" d="M78.8,10L64,35.4L49.2,10H0l64,110l64-110C128,10,78.8,10,78.8,10z" />
-                <path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z" />
-            </svg> -->
+            <svg class="logo" width="24" height="24" viewBox="0 0 426 372" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M223.392 366C218.773 374 207.226 374 202.608 366L1.68978 18C-2.92902 9.99998 2.84449 0 12.0821 0L413.918 0C423.156 0 428.929 10 424.31 18L223.392 366Z"
+                    fill="url(#paint0_linear_158_112)" />
+                <defs>
+                    <linearGradient id="paint0_linear_158_112" x1="451" y1="23" x2="-1.99999" y2="245"
+                        gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#7A85EC" />
+                        <stop offset="1" stop-color="#3DDCFF" />
+                    </linearGradient>
+                </defs>
+            </svg>
             <span class="text">Volar.js</span>
         </slot>
     </a>
