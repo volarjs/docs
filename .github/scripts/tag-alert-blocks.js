cdb2b8e7677da7ffbe797bb48a52767b4ef6d8f0
Princesseuh <princssdev@gmail.com>
2023-07-19 01:53:06 +0200

    docs: add page for Volar Labs

--- .github/scripts/tag-alert-blocks.js
+++ .github/scripts/tag-alert-blocks.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process')
+const { exec } = require("child_process");
 
 /**
  * Execute a command and return stdout as string.
@@ -9,34 +9,34 @@ const { exec } = require('child_process')
  */
 function run(command) {
   return new Promise((resolve, reject) => {
-    exec(command, { encoding: 'utf-8' }, (error, stdout) =>
-      error ? reject(error) : resolve(stdout)
-    )
-  })
+    exec(command, { encoding: "utf-8" }, (error, stdout) =>
+      error ? reject(error) : resolve(stdout),
+    );
+  });
 }
 
-const ALERT_BLOCK = /^\+\s*:::\s?(\w+)/m
+const ALERT_BLOCK = /^\+\s*:::\s?(\w+)/m;
 
-async function isUsingAlertBlock(base = 'origin/master') {
-  const result = await run(`git diff --name-only ${base}`)
+async function isUsingAlertBlock(base = "origin/master") {
+  const result = await run(`git diff --name-only ${base}`);
   const files = (
     await Promise.all(
       result
         .trim()
         .split(/\r?\n/)
-        .map(file =>
+        .map((file) =>
           run(`git diff ${base} -- ${file}`)
-            .then(diff => ALERT_BLOCK.test(diff))
-            .then(usesAlertBlock => (usesAlertBlock ? file : ''))
-        )
+            .then((diff) => ALERT_BLOCK.test(diff))
+            .then((usesAlertBlock) => (usesAlertBlock ? file : "")),
+        ),
     )
-  ).filter(Boolean)
+  ).filter(Boolean);
 
   if (files.length) {
-    return true
+    return true;
   }
 
-  return false
+  return false;
 }
 
-module.exports = { isUsingAlertBlock }
+module.exports = { isUsingAlertBlock };
--- astro.config.ts
+++ astro.config.ts
@@ -1,40 +1,41 @@
-import starlight from '@astrojs/starlight';
-import { defineConfig, sharpImageService } from 'astro/config';
+import starlight from "@astrojs/starlight";
+import { defineConfig, sharpImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-		starlight({
-			logo: {
-				src: './src/assets/logo.svg',
-				alt: "Volar's logo, a light-blue prism with a very subtle tint of red in the top right and a wave going through the prism.",
-			},
-			title: 'Volar.js',
-			description: "The Embedded Language Tooling Framework",
+    starlight({
+      logo: {
+        src: "./src/assets/logo.svg",
+        alt: "Volar's logo, a light-blue prism with a very subtle tint of red in the top right and a wave going through the prism.",
+      },
+      title: "Volar.js",
+      description: "The Embedded Language Tooling Framework",
       social: {
-				github: 'https://github.com/volarjs/',
-				twitter: 'https://twitter.com/johnsoncodehk'
+        github: "https://github.com/volarjs/",
+        twitter: "https://twitter.com/johnsoncodehk",
       },
       sidebar: [
         {
-          label: 'Core Concepts',
+          label: "Core Concepts",
           items: [
             // Each item here is one entry in the navigation menu.
-						{ label: 'Why Volar?', link: '/core-concepts/why-volar' },
-						{ label: 'Embedded Languages', link: '/core-concepts/embedded-languages' },
-					],
-					// TODO: Use `autogenerate` once it allows you to order the sidebar
-				},
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Your First Volar Language Server', link: '/guides/first-server' },
-						{ label: 'File Structure', link: '/guides/file-structure' }
-					]
-				},
+            { label: "Why Volar?", link: "/core-concepts/why-volar" },
+            { label: "Embedded Languages", link: "/core-concepts/embedded-languages" },
+            { label: "Volar Labs", link: "/core-concepts/volar-labs" },
+          ],
+          // TODO: Use `autogenerate` once it allows you to order the sidebar
+        },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: "Guides",
+          items: [
+            { label: "Your First Volar Language Server", link: "/guides/first-server" },
+            { label: "File Structure", link: "/guides/file-structure" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
         },
       ],
     }),
